04b6d5b2e15d224d7b8be3826a69ff0fde70df37
Marco <rubolini.marco@gmail.com>
2020-05-12 16:47:33 +0200

    Funzione risposta pc

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
 // intercetto il click dell'utente
 $('.fa-microphone').on("click", inviaMessaggio);
 
-$('#content-message').keypress(function(e){
-    if (e.which == 13) {
+
+$('.fa-smile').click(function(){
+    rispostaPc();
+})
+// intercetto il pulsante invio della tastiera
+$('#content-message').keypress(function(event){
+    if (event.which == 13) {
         inviaMessaggio();
     }
 });
 
+// Cerca nome contatto
+
+// intercetto il click dell'utente sul pulsante ricerca
+
+$('.search').click(function(){
+    alert('ciao');
+});
+
+
+// funzione invia messaggio utente
 
 function inviaMessaggio (){
     var contentMessage = $('#content-message').val();
 
-    // copio l'elemento template
+    if (contentMessage.trim() != '') {
+        // copio l'elemento template
+        var message = $('.template-message .message.send').clone();
+
+        // inserisco il testo inserito nell'input dall'utente
+
+        message.find('.text').text(contentMessage);
+
+        // appendo il nuovo messaggio
+        $('.conversation').append(message);
+
+        $("#content-message").val("");
+
+        setTimeout(risposta, 1000);
+    };
+};
+
+//Funzione di risposta automatica del pc
+function risposta (){
+    var risposta = "ok";
     var message = $('.template-message .message.reply').clone();
-
-    // inserisco il testo inserito nell'input dall'utente
-
-    message.find('.text').text(contentMessage);
-
-    // appendo il nuovo messaggio
+    message.find('.text').text(risposta);
     $('.conversation').append(message);
-
-    $("#content-message").val("");
 };
